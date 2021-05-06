5a48584a44326d2565cfbc2aa7cc2b2c94f2dab0
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-05-06 12:22:26 +1200

    Component 1 Beginning of Development
    This is the beginning of Component 1 Development.

--- 01_Start_GUI.py
+++ 01_Start_GUI.py
@@ -2,3 +2,18 @@
 # This file will become Component 1 of the "Fear Quiz Assessment".
 # [Note: This file has now been edited on a new device, 
 # ... so a new build of the code can now be marked as currently active]
+
+# Import Statements as follows (from "01_Mystery_Box_Outline.py"):
+from tkinter import *
+import random
+from functools import partial # This prevents unwanted windows from opening
+
+# Start Class 
+
+
+# Main Routine (edited from "02_Start_GUI.py")
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Fear Quiz")
+    something = Start(root)
+    root.mainloop()
