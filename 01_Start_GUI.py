e17c84982a92f38d9557b308bf211585524edbc5
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-05-07 10:41:57 +1200

    Working Start GUI format with Trialing documented
    This is a build of the project that contains a working Start GUI format.

--- 01_Start_GUI.py
+++ 01_Start_GUI.py
@@ -8,8 +8,42 @@ from tkinter import *
 import random
 from functools import partial # This prevents unwanted windows from opening
 
-# Start Class 
+# Start Class (with various portions taken from "01_Mystery_Box_Outline.py")
+class Start:
+    def __init__(self, parent):
 
+        # Initialising Start Frame (from "01_Mystery_Box_Outline.py")
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        # Start Heading (Row 0)
+        self.quiz_heading_label = Label(self.start_frame, text="Fear Quiz", font="Arial 25 bold", justify=CENTER, padx=10, pady=10)
+        self.quiz_heading_label.grid(row=0)
+
+        # Start Instructions (Row 1)
+        self.start_instructions = Label(self.start_frame, text="Please enter the number of questions that you would like to answer in the box below.", 
+                                        font="Arial 10 italic", justify=LEFT, padx=10, pady=10, wrap=350)
+        self.start_instructions.grid(row=1)
+
+        # Number Entry Box (Row 2) [from "01_Mystery_Box_Outline.py"]
+        self.number_entry_box = Entry(self.start_frame, width=50)
+        self.number_entry_box.grid(row=2)
+
+        # Start Error Message Area (Row 3)
+        self.start_error_message_area = Label(self.start_frame, text="")
+        self.start_error_message_area.grid(row=3)
+
+        # Start Buttons Frame (Row 4) [inspired by "08b_Game_Export_GUI_Version_2.py" and "05_Game_Playable.py"]
+        self.start_buttons_frame = Frame(pady=15)
+        self.start_buttons_frame.grid(row=4)
+
+        # Help Button (Row 0, Column 0)
+        self.help_button = Button(self.start_buttons_frame, text="Help", font="Arial 10", pady=5, padx=10, bg="orange")
+        self.help_button.grid(row=0, column=0, padx=50)
+
+        # Start Button (Row 0, Column 1)
+        self.start_button = Button(self.start_buttons_frame, text="Start", font="Arial 10", pady=5, padx=10, bg="green")
+        self.start_button.grid(row=0, column=1, padx=50)
 
 # Main Routine (edited from "02_Start_GUI.py")
 if __name__ == "__main__":
